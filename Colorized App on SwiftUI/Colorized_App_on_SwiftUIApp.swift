69af96ab3efcae2cf5406c14d7e0bffaf4aba8a3
tuffdeblain <himanfullhd@gmail.com>
2021-09-12 21:57:22 +0700

    Initial Commit

---
+++ Colorized App on SwiftUI/Colorized_App_on_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorized_App_on_SwiftUIApp.swift
+//  Colorized App on SwiftUI
+//
+//  Created by Сергей Кудинов on 12.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Colorized_App_on_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorized App on SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colorized App on SwiftUI
+//
+//  Created by Сергей Кудинов on 12.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
